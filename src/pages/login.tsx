18ab3a8c0c9a6afc214ae9e373a4efad8413e05b
kurihara.sk <nikoand01@gmail.com>
2023-01-01 18:04:49 +0900

    clean: replace some of root files to src folder

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,23 @@
 import { getAuth, signInWithPopup } from "firebase/auth";
 import React from "react";
-import { provider } from "../firebase/init";
+import { provider } from "./firebase/init";
 const auth = getAuth();
 export const Login = () => {
+  console.log(auth.currentUser);
   const LoginWithGoogle = () => {
     // Googleでログイン
     signInWithPopup(auth, provider).then((result) => {
       console.log(result);
     });
   };
+  const checkCurrentUser = () => {
+    console.log(auth.currentUser, "ウエイ");
+  };
   return (
     <>
       <div>Login</div>
       <button onClick={LoginWithGoogle}>Googleでログイン</button>
+      <button onClick={checkCurrentUser}>ログイン確認</button>
     </>
   );
 };
