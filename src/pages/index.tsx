36a557c168814b2e04e2383436860954ccf7d177
kurihara.sk <nikoand01@gmail.com>
2023-02-03 19:46:50 +0900

    fix: some refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
 import Router from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchSessionCheck } from "../apis/auth";
 import LoginButton from "../components/auth/loginButton";
 import SignupButton from "../components/auth/signupButton";
+import { isLoggedIn } from "../utils/auth";
+import { calendarPath, homePath } from "../utils/routes";
 
 const Home = () => {
+  useEffect(() => {
+    (async () => {
+      await fetchSessionCheck().then((data) => data.currentUser);
+      const loggedIn = isLoggedIn();
+      if (!loggedIn) {
+        Router.push(homePath);
+      } else {
+        Router.push(calendarPath);
+      }
+    })();
+  }, []);
   return (
     <>
       <div>ホームだぜ</div>
